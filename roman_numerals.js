3294bf339e885e3a58820fe46fae221c1c62db34
taqiabdulaziz <svx.annexiv@gmail.com>
2018-11-26 13:54:44 +0700

    komit

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,43 @@
-function to_roman (num) {
-  // your implementation code here
+function to_roman(num) {
+  const ribuan = ["", "M", "MM", "MMM"]
+  const ratusan = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+  const puluhan = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+  const satuan = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+  const numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  let numberStr = String(num)
+  let result = undefined
+  let index1 = undefined
+  let index2 = undefined
+  let index3 = undefined
+  let index4 = undefined
+
+  switch (numberStr.length) {
+    case 1:
+      index = numbersArr.indexOf(numberStr[0])
+      return satuan[index]
+      break;
+
+    case 2:
+      index1 = numbersArr.indexOf(numberStr[0])
+      index2 = numbersArr.indexOf(numberStr[1])
+      return puluhan[index1] + satuan[index2]
+      break;
+
+    case 3:
+      index1 = numbersArr.indexOf(numberStr[0])
+      index2 = numbersArr.indexOf(numberStr[1])
+      index3 = numbersArr.indexOf(numberStr[2])
+      return ratusan[index1] + puluhan[index2] + satuan[index3]
+      break;
+
+    case 4:
+      index1 = numbersArr.indexOf(numberStr[0])
+      index2 = numbersArr.indexOf(numberStr[1])
+      index3 = numbersArr.indexOf(numberStr[2])
+      index4 = numbersArr.indexOf(numberStr[3])
+      return ribuan[index1] + ratusan[index2] + puluhan[index3] + satuan[index4]
+      break;
+  }
 }
 
 // Drive code
