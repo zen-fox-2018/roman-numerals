4d69b2f6ce39d403e8170a47ea33129ca8fa0faa
ahmad <ahmad.syukron6991@gmail.com>
2018-11-26 18:50:05 +0700

    romawi done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,64 @@
 function to_roman (num) {
-  // your implementation code here
+  result = ''
+var cek = num
+   for (i = num ; i >= 0; i --){
+     if(i === 1000){
+       result += 'M'
+       cek = num - 1000
+       i = cek
+
+     }else if( i === 500){
+       result+='D'
+       cek = cek - 500
+       i = cek
+     }
+     else if (i === 400 && num <= 1500){
+       result += 'CD'
+       cek = cek - 400
+       i = cek
+     }
+     else if(i === 100 && i <= 400){
+       result += 'C'
+       cek = cek - 100
+       i = cek
+     }else if(i === 50){
+       result += 'L'
+       cek = cek - 50  
+       i = cek
+       console.log(i)
+
+     }
+     else if(i === 40 && i < 50){
+       result += 'XL'
+       cek = cek - 40
+       i = cek
+     }
+     else if(i === 10){
+      result += 'X'
+      cek = cek - 10
+      i = cek
+     } else if(i === 9){
+      result += 'IX'
+      cek = cek - 9
+      i = cek
+     }
+     else if (i === 5 ){
+       result += 'V'
+       cek = cek - 5
+       i = cek
+       console.log(i)
+     }
+     else if( i === 4){
+       result += 'IV'
+       cek = cek - 4
+       i = cek
+     }else if (i < 4){
+       result += 'I'
+     
+     }
+   
+  }
+   return result
 }
 
 // Drive code
