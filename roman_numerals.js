694cfc34278b762cae38070ebd3d9b1cf440fdae
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-11-26 13:59:45 +0700

    done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,28 @@
 function to_roman (num) {
-  // your implementation code here
+  let kamus = {
+    M: 1000,
+    CM : 900,
+    D: 500,
+    CD:400 ,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  }
+
+  let result = ''
+  for(i in kamus) {
+    while(num >= kamus[i]) {
+      result += i
+      num -= kamus[i]
+    }
+  }
+  return result
 }
 
 // Drive code
