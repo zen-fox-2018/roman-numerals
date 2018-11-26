cc36c9f192150ea990b7a4042b4e7e5cf605c380
rahmanhasri <rahman.hasri@gmail.com>
2018-11-26 23:11:56 +0700

    roman

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,6 +1,85 @@
-function to_roman (num) {
-  // your implementation code here
+// function to_roman(num) {
+//   let arabic = [1000, 900, 500, 400, 100 , 90, 50, 40, 10, 9, 5, 4, 1]
+//   let roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+//   let output = ''
+//   while (num !== 0){
+//
+//     for (let i = 0 ; i < arabic.length ; i++){
+//       if (num >= arabic[i]){
+//         // console.log(i, '>',num >= arabic[i])
+//         output += roman[i]
+//         num -= arabic[i]
+//         break;
+//       }
+//     }
+//   }
+//
+//   return output
+//
+// }
+
+
+
+function to_roman(num){
+  let output = ''
+  while (num !== 0){
+
+    if (num < 10){
+      if (num < 4){
+        output += 'I'
+        num--
+      } else if (num === 4){
+        output += 'IV'
+        num -= 4
+      } else if (num < 9){
+        output += 'V'
+        num -= 5
+      } else if (num === 9){
+        output+= 'IX'
+        num -= 9
+      }
+
+    } else if (num < 100){
+      if (num < 40){
+        num -= 10
+        output += 'X'
+      } else if (num < 50){
+        output += 'XL'
+        num -= 40
+      } else if (num < 90){
+        output += 'L'
+        num -= 50
+      } else if (num >= 90){
+        output += 'XC'
+        num -= 90
+      }
+
+    } else if (num < 1000) {
+      if (num < 400){
+        output += 'C'
+        num -= 100
+      } else if (num < 500){
+        output += 'CD'
+        num -= 400
+      } else if (num < 900){
+        output += 'D'
+        num -= 500
+      } else if (num >= 900){
+        output += 'CM'
+        num -= 900
+      }
+
+    } else {
+      output += 'M'
+      num -= 1000
+    }
+
+  }
+
+  return output
+
 }
+
 
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
