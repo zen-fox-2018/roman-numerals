d7794447000a37115e79fee745afaf79d8fa304d
patriagani <patria.gani@gmail.com>
2018-11-26 17:33:36 +0700

    roman numerals (findIndex) done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,29 +1,17 @@
 function to_roman (num) {
-  let ribuanDic = ["","M","MM","MMM"]
-  let ratusanDic = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
-  let puluhanDic = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
-  let satuanDic = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
-  let numberDic = ["0","1","2","3","4","5","6","7","8","9"]
+  let romanDic = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+  let numberDic = [1000,900,500,400,100,90,50,40,10,9,5,4,1 ]
+  let number = num
   let result = ""
 
-  let numStr = String(num)
-
-  if (numStr.length === 4) {
-    result = ribuanDic[numberDic.indexOf(numStr[0])] + ratusanDic[numberDic.indexOf(numStr[1])] +
-             puluhanDic[numberDic.indexOf(numStr[2])] + satuanDic[numberDic.indexOf(numStr[3])]
+  while (number > 0) {
+    let index = numberDic.findIndex(function(element) {
+      return element <= number
+    })
+    result = result + romanDic[index]
+    number = number - numberDic[index]
   }
-  else if (numStr.length === 3) {
-    result = ratusanDic[numberDic.indexOf(numStr[0])] + puluhanDic[numberDic.indexOf(numStr[1])] +
-             satuanDic[numberDic.indexOf(numStr[2])]
-  }
-  else if (numStr.length === 2) {
-    result = puluhanDic[numberDic.indexOf(numStr[0])] + satuanDic[numberDic.indexOf(numStr[1])]
-  }
-  else if (numStr.length === 1) {
-    result = satuanDic[numberDic.indexOf(numStr[0])]
-  }
-
-  return (result);
+  return result
 }
 
 // Drive code
