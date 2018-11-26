04e5b593a7464703667ecf6f5850c2991102afa8
fauzan_bintang <obin.aji@gmail.com>
2018-11-26 18:52:37 +0700

    done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,6 +1,28 @@
+const roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
+const number = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+
 function to_roman (num) {
-  // your implementation code here
+    let result = ''
+    let i = number.length - 1
+    while (num) {
+        if (num - number[i] >= 0) {
+            result += roman[i]
+            num -= number[i]
+        } else {
+            i--
+        }
+    }
+    return result
 }
+
+// // pake .findIndex
+// function to_roman (num) {
+//     console.log(roman.findIndex(findNumber(num)));
+    
+//     // while (num) {
+//     // }
+// }
+
 
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
