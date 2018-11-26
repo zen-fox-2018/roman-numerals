d10e2395c30bde8022fdec972e96345eda9576e7
patriagani <patria.gani@gmail.com>
2018-11-26 13:00:38 +0700

    roman numerals done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,29 @@
 function to_roman (num) {
-  // your implementation code here
+  let ribuanDic = ["","M","MM","MMM"]
+  let ratusanDic = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
+  let puluhanDic = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
+  let satuanDic = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
+  let numberDic = ["0","1","2","3","4","5","6","7","8","9"]
+  let result = ""
+
+  let numStr = String(num)
+
+  if (numStr.length === 4) {
+    result = ribuanDic[numberDic.indexOf(numStr[0])] + ratusanDic[numberDic.indexOf(numStr[1])] +
+             puluhanDic[numberDic.indexOf(numStr[2])] + satuanDic[numberDic.indexOf(numStr[3])]
+  }
+  else if (numStr.length === 3) {
+    result = ratusanDic[numberDic.indexOf(numStr[0])] + puluhanDic[numberDic.indexOf(numStr[1])] +
+             satuanDic[numberDic.indexOf(numStr[2])]
+  }
+  else if (numStr.length === 2) {
+    result = puluhanDic[numberDic.indexOf(numStr[0])] + satuanDic[numberDic.indexOf(numStr[1])]
+  }
+  else if (numStr.length === 1) {
+    result = satuanDic[numberDic.indexOf(numStr[0])]
+  }
+
+  return (result);
 }
 
 // Drive code
