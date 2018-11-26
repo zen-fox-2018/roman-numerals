76f9b6eac26259f38d58d10a1c08192a0a42973a
ACER <fauziaazia171097@gmail.com>
2018-11-26 02:11:01 -0500

    finish

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,4 +1,30 @@
 function to_roman (num) {
+  const Roman = {
+    M : 1000,
+    CM :900,
+    D:500,
+    CD:400,
+    C:100,
+    XC:90,
+    L:50,
+    XL:40,
+    X:10,
+    IX:9,
+    V:5,
+    IV:4,
+    I:1
+}
+let result = ''
+
+    for( i in Roman){
+        // console.log(Roman[i])
+        while( num >= Roman[i]){
+            num -= Roman[i]
+            result+= i
+        }
+    }
+
+return result
   // your implementation code here
 }
 
