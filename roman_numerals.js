795ae2da4da9df8c74d1fc3678e173e0667c8e92
Aditya Respati <AdityaRespati>
2018-11-26 21:17:46 +0700

    added a new file

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,53 @@
-function to_roman (num) {
+function to_roman(num) {
   // your implementation code here
+  var output = ""
+  var strNum = String(num)
+  var one = ''
+  var five = ''
+  var ten = ''
+
+  for (var i = 0; i <= strNum.length; i++) {
+    if (strNum.length - i === 4) {
+      one = "M"
+    }
+    else if (strNum.length - i === 3) {
+      one = "C"
+      five = "D"
+      ten = "X"
+    }
+    else if (strNum.length - i === 2) {
+      one = "X"
+      five = "L"
+      ten = "C"
+    }
+    else if (strNum.length - i === 1) {
+      one = "I"
+      five = "V"
+      ten = "X"
+    }
+
+    if (Number(strNum[i]) < 4) {
+      for (var j = 0; j < Number(strNum[i]); j++) {
+        output += one
+      }
+    }
+    else if (Number(strNum[i]) == 4) {
+      output += one + five
+    }
+    else if (Number(strNum[i]) == 5) {
+      output += five
+    }
+    else if (Number(strNum[i]) > 5 && Number(strNum[i]) < 9) {
+      output += five
+      for (var k = 6; k <= Number(strNum[i]); k++) {
+        output += one
+      }
+    }
+    else if (Number(strNum[i] == 9)) {
+      output += one + ten
+    }
+  }
+  return output
 }
 
 // Drive code
